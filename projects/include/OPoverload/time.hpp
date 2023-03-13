c97e77bc1a19636d9d8aeb616c0ec0c76235b46b
NievinEvgeny <zhenya-nievin@mail.ru>
2023-03-13 08:55:12 +0700

    Add +,-,<< operators for time classes

--- projects/include/OPoverload/time.hpp
+++ projects/include/OPoverload/time.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 namespace timelib {
 
@@ -28,5 +29,59 @@ class TimeSpan : public TimeInterface
     {
     }
 };
+
+inline TimeSpan operator-(const Time& lhs, const Time& rhs)
+{
+    if (lhs.time_ - rhs.time_ > lhs.time_)
+    {
+        throw std::overflow_error{"result time below zero"};
+    }
+    return TimeSpan(lhs.time_ - rhs.time_);
+}
+
+inline Time operator+(const Time& lhs, const TimeSpan& rhs)
+{
+    if (lhs.time_ - rhs.time_ < lhs.time_)
+    {
+        throw std::overflow_error{"overflow"};
+    }
+    return Time(lhs.time_ + rhs.time_);
+}
+
+inline Time operator-(const Time& lhs, const TimeSpan& rhs)
+{
+    if (lhs.time_ - rhs.time_ > lhs.time_)
+    {
+        throw std::overflow_error{"result time below zero"};
+    }
+    return Time(lhs.time_ - rhs.time_);
+}
+
+inline TimeSpan operator+(const TimeSpan& lhs, const TimeSpan& rhs)
+{
+    if (lhs.time_ - rhs.time_ < lhs.time_)
+    {
+        throw std::overflow_error{"overflow"};
+    }
+    return TimeSpan(lhs.time_ + rhs.time_);
+}
+
+inline TimeSpan operator-(const TimeSpan& lhs, const TimeSpan& rhs)
+{
+    if (lhs.time_ - rhs.time_ > lhs.time_)
+    {
+        throw std::overflow_error{"result time below zero"};
+    }
+    return TimeSpan(lhs.time_ - rhs.time_);
+}
+
+inline std::ostream& operator<<(std::ostream& os, const TimeInterface& val)
+{
+    std::size_t day = val.time_ / (86400);
+    std::size_t hour = (val.time_ - day * 86400) / 3600;
+    std::size_t min = (val.time_ - day * 86400 - hour * 3600) / 60;
+    std::size_t sec = (val.time_ - day * 86400 - hour * 3600 - min * 60);
+    return os << day << "d " << hour << "h " << min << "m " << sec << "s " << '\n';
+}
 
 }  // namespace timelib
--- projects/src/OPoverload/main.cpp
+++ projects/src/OPoverload/main.cpp
@@ -1,7 +1,21 @@
 #include <OPoverload/time.hpp>
+#include <iostream>
 
 int main()
 {
-    timelib::Time(5);
-    timelib::TimeSpan(100);
+    try
+    {
+        const std::size_t num2 = 432000;
+        const std::size_t num1 = 86401;
+
+        timelib::Time time(num1);
+        timelib::TimeSpan time_span(num2);
+
+        std::cout << time - time_span << '\n';
+    }
+    catch (const std::exception& msg)
+    {
+        std::cerr << msg.what() << '\n';
+        return -1;
+    }
 }
